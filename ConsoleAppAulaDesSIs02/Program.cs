2840c1aee8ba84f8fbe629c79b9af3fe0d0f6fd2
Usuario <Usuario@CPD03-M04>
2024-02-27 12:22:13 -0300

    inicio do teste c#

--- ConsoleAppAulaDesSIs02/Program.cs
+++ ConsoleAppAulaDesSIs02/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -30,6 +31,9 @@ namespace ConsoleAppAulaDesSIs02
 
             // fim do segundo ex e inicio do terceiro
 
+            //é possivel fazer declarando primeiro as variaveis primeiros,vc usa o double e escreve as
+            //variaveis separando -as com virgulas
+
             Console.WriteLine("Escreva quanto está o Litro do combustível: ");
             double gasolina = double.Parse( Console.ReadLine() );
 
@@ -42,7 +46,39 @@ namespace ConsoleAppAulaDesSIs02
             double ValorViagem = (dist / km) * gasolina ;
 
             Console.WriteLine("O valor da viagem é de: " +  ValorViagem);
-            //
+
+            // término do terceiro e exercício e unicio do quarto
+
+            Console.WriteLine("Escreva sua idade em anos: ");
+             int anos = int.Parse( Console.ReadLine() );
+
+            Console.WriteLine("Escreva quantos meses passou desde de seu ultimo aniversário: ");
+            int meses = int.Parse( Console.ReadLine() );
+
+            Console.WriteLine("Escreva quantos dias se passaram do mes: ");
+            int dias = int.Parse( Console.ReadLine() );
+
+            int r3 = (anos * 365) + (meses * 30) + dias;
+            Console.WriteLine("seus dias vividos são de: "+ r3);
+
+            //fim do quarto exercicio e inicio do quinto
+            //esse ex. é pedido que criemos duas variáveis e troquemos o valor de uma pela outra mas sem mudar as variáveis
+
+            Console.WriteLine("digite um valor para A: ");
+            double A = double.Parse( Console.ReadLine() );
+
+            Console.WriteLine("Digite um valor para B: ");
+            double b = double.Parse( Console.ReadLine() );
+            double c;
+            c = A;
+             A = b;
+            b = c;
+
+
+            Console.WriteLine("O valor de A é de : "+ A);
+            Console.WriteLine("O valor de B é de : " + b);
+
+
 
 
 
